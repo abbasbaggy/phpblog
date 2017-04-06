f1c3bb3d8a3971878b54bdcf6984ac8dae0d18a3
abbasbaggy <a.yusuff@rgu.ac.uk>
2017-04-06 18:20:44 +0100

    Initial commit

---
+++ index.php
@@ -0,0 +1,32 @@
+<?php ini_set('display_errors', 1);
+
+define('INCLUDE_DIR', dirname("home.php") . '/inc/');
+
+$rules = array(
+    //main pages
+    'about'=>"/about",
+    'contactus'=>"/contactus",
+    'blog'=>"/blog",
+    'blog_article'=>"/blog/(?'blogID'[\w\-]+)",
+
+    //Admin pages
+    'login'=>"/login",
+    'create_article'=>"/createarticle",
+    'logout'=>"/logout",
+
+    //Home page
+    'home'=>"home"
+);
+
+$uri = rtrim(dirname($_SERVER["SCRIPT_NAME"]), '/');
+$uri = '/' . trim(str_replace($uri, '', $_SERVER['REQUEST_URI']), '/');
+$uri = urldecode($uri);
+
+foreach ($rules as $action => $rule) {
+    if (preg_match('~^' . $rule . '$~i', $uri, $params)) {
+        include(INCLUDE_DIR. $action . '.php');
+        exit();
+    }
+}
+//nothing is found so handle as 404 error
+include(INCLUDE_DIR . '404.php');
