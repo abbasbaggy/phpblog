efa80424789f819e26397f7b20b27429e800aa9d
abbasbaggy <a.yusuff@rgu.ac.uk>
2017-05-03 15:24:43 +0100

    api

--- inc/create_article.php
+++ inc/create_article.php
@@ -5,9 +5,7 @@
  * Date: 3/18/2017
  * Time: 11:51 PM
  */
-session_start();
-if (isset($_SESSION['username']))  //if session does exist
-{
+
     if ($_SERVER['REQUEST_METHOD'] === 'GET') {
 
         include("scripts/header.php");
@@ -40,7 +38,7 @@ if (isset($_SESSION['username']))  //if session does exist
         header("blog");
     }
     //test
-} else {
+ else {
     header("location:login");
 }
 ?>
