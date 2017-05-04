40ed71a4a51c352dbd14ef3cdc4af92baf5bdc78
abbasbaggy <a.yusuff@rgu.ac.uk>
2017-05-04 20:32:36 +0100

    api

--- inc/blog.php
+++ inc/blog.php
@@ -24,7 +24,9 @@ while($row = $result->fetch_array())
 
     echo "<li><a href='blog/{$articleID}'>{$articleName}</a> by {$articleAuthor}</li>";
     echo "<li><a href='api/{$articleID}'>{$articleName}</a> by {$articleAuthor}</li>";
-    echo json_encode($articleID,$articleName,$articleAuthor);
+    echo json_encode($articleID);
+    echo json_encode($articleAuthor);
+    echo json_encode($articleName)
 }
 echo "
 </main>";
