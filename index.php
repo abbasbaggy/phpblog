9eb300edf9ffdb878d9d662141316f6e40299748
abbasbaggy <a.yusuff@rgu.ac.uk>
2017-05-05 15:19:36 +0100

    api

--- index.php
+++ index.php
@@ -17,7 +17,7 @@ $rules = array(
     'logout'=>"/logout",
     'api'=>"/api/(?'blog2D'[\w\-]+)",
     'api2'=>"/api2",
-    'api3'=>"/api/(?'blog2D'[\w\-]+)/update",
+    'api3'=>"/api(?'blog2D'[\w\-]+)/",
 
     //Home page
     'home'=>"/"
