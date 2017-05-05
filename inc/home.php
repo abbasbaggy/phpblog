bc9e487a8e51bdf95e20f494204181d98296d34f
abbasbaggy <a.yusuff@rgu.ac.uk>
2017-05-05 16:55:28 +0100

    api

--- inc/home.php
+++ inc/home.php
@@ -12,7 +12,7 @@ echo"
 <p>This is abbas Api </p>
 <p>to GET all data 'http://webdevrgu.azurewebsites.net/api' to the address bar</p>
 <p>to POST a data 'http://webdevrgu.azurewebsites.net/api2' to the address bar</p>
-<p>to PUT (update data) 'http://webdevrgu.azurewebsites.net/api3'name' to the address bar</p>
+<p>to PUT (update data), 'http://webdevrgu.azurewebsites.net/api3'name' to the address bar</p>
 
 
 </main>
